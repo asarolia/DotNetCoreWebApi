ad0cb1714269c07144be86ed2b17be776ad20285
Abhishek Sarolia <sarolia4u@gmail.com>
2019-01-30 18:17:38 +0000

    Added API support for PUT and DELETE

--- DotnetCoreWebApiBP/Controllers/ToDoController.cs
+++ DotnetCoreWebApiBP/Controllers/ToDoController.cs
@@ -57,6 +57,38 @@ namespace DotnetCoreWebApiBP.Controllers
             return CreatedAtAction(nameof(GetToDoItem), new { id = item.Id }, item);
         }
 
-        
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoItem(long id)
+        {
+            var todoItem = await _context.ToDoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
     }
 }
